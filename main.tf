fac3cd275aefc77b946b7fcd4cc707e02b4077bf
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-02-19 15:35:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,322 @@
+###############################################################################
+# Terraform Configuration
+###############################################################################
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
+  }
+}
+
+###############################################################################
+# AWS Provider
+###############################################################################
+provider "aws" {
+  region = var.region
+}
+
+###############################################################################
+# Random Cluster Name
+###############################################################################
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+locals {
+  cluster_name = "karpenter-${random_string.suffix.result}"
+}
+
+###############################################################################
+# Availability Zones
+###############################################################################
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+###############################################################################
+# VPC
+###############################################################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "education-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+###############################################################################
+# EKS Cluster
+###############################################################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+  enable_irsa                              = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa_ebs_csi.iam_role_arn
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
+
+  eks_managed_node_groups = {
+    frontend = {
+      instance_types = ["t3.small"]
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      labels         = { app = "reactjs" }
+    }
+
+    java-backend = {
+      instance_types = ["t3.small"]
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      labels         = { app = "java" }
+    }
+  }
+}
+
+###############################################################################
+# EBS CSI IRSA
+###############################################################################
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa_ebs_csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKS_EBS_CSI_${local.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+###############################################################################
+# Helm Provider (After EKS)
+###############################################################################
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        var.region
+      ]
+    }
+  }
+}
+
+
+
+###############################################################################
+# AWS Load Balancer Controller IRSA
+###############################################################################
+module "irsa_aws_lb_controller" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.39.0"
+
+  role_name = "aws-load-balancer-controller-${local.cluster_name}"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+###############################################################################
+# AWS Load Balancer Controller Helm
+###############################################################################
+rresource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set = [
+    { 
+      name  = "clusterName"
+      value = module.eks.cluster_name
+    },
+    { 
+      name  = "region"
+      value = var.region
+    },
+    { 
+      name  = "vpcId"
+      value = module.vpc.vpc_id
+    },
+    { 
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    { 
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    { 
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.irsa_aws_lb_controller.iam_role_arn
+    }
+  ]
+
+  depends_on = [
+    module.eks,
+    module.irsa_aws_lb_controller
+  ]
+}
+
+
+
+###############################################################################
+# External DNS IRSA
+###############################################################################
+module "irsa_external_dns" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.39.0"
+
+  role_name = "external-dns-${local.cluster_name}"
+
+  attach_external_dns_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+}
+
+###############################################################################
+# External DNS Helm
+###############################################################################
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+
+  set = [
+  { 
+    name  = "provider"
+    value = "aws"
+  },
+  { 
+    name  = "policy"
+    value = "sync"
+  },
+  { 
+    name  = "registry"
+    value = "txt"
+  },
+  { 
+    name  = "txtOwnerId"
+    value = module.eks.cluster_name
+  },
+  { 
+    name  = "serviceAccount.create"
+    value = "true"
+  },
+  { 
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  },
+  { 
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.irsa_external_dns.iam_role_arn
+  }
+]
+
+
+  depends_on = [
+    module.eks,
+    module.irsa_external_dns
+  ]
+}
+
+
+###############################################################################
+# Metrics Server Helm
+###############################################################################
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+
+  set = [
+    { 
+      name  = "args"
+      value = "{--kubelet-insecure-tls}"
+    }
+  ]
+
+  depends_on = [module.eks]
+}
