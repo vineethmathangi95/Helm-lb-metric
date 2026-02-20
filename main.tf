ff782d4e54d2fb227656d82f651897cc6f05c909
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-02-20 14:30:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -131,6 +131,21 @@ module "eks" {
 }
 
 ###############################################################################
+# EKS Cluster Auto Scaler
+###############################################################################
+
+module "cluster_autoscaler" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-cluster-autoscaler.git"
+
+  enabled = true
+
+  cluster_name                     = module.eks.cluster_id
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  aws_region                       = data.aws_region.current.name
+}
+
+###############################################################################
 # EBS CSI IRSA
 ###############################################################################
 data "aws_iam_policy" "ebs_csi_policy" {
